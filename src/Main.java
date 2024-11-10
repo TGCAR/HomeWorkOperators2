c7ad00004f3b30d0d541a350e54a3d9eb2bfcc09
TGCAR <don.meln2013@yandex.ru>
2024-11-10 17:49:05 +0300

    HomeWorkOperators2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 1;
+
+        // Задача 1
+
+        int clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        // Задача 2
+
+        int clientDevicesYear = 2010;
+        if (clientOS == 0 && clientDevicesYear <= 2015) {
+            System.out.println("Установите блегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDevicesYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        // Задача 3
+
+
     }
 }
