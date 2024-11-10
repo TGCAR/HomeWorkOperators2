aee8539e8ef94d0fbe97d690cc375fce900d371c
TGCAR <don.meln2013@yandex.ru>
2024-11-10 10:28:56 +0300

    HomeWorkOperators2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
-
-
-
 }
