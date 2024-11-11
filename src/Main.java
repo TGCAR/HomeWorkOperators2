34934ac9fa6e50c2561fe569df5629dbfc0b08e7
TGCAR <don.meln2013@yandex.ru>
2024-11-11 09:53:19 +0300

    HomeWorkOperators2

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,47 @@ public class Main {
 
         // Задача 4
 
-        int
+
+        int deliveryDistance = 101;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: " + 2);
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: " + 3);
+        } else {
+            System.out.println("Доставки нет");
+        }
+
+        // Задача 5
+
+        int monthNumber = 12;
+
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+
+        }
     }
 }
 
